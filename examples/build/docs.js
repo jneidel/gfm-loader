a389c174fba216b857bbf45fe1d4cf6253660b6c
Jonathan Neidel <github@jneidel.com>
2018-09-05 19:28:43 +0500

    Remove build files

--- examples/build/docs.js
+++
@@ -1,9 +0,0 @@
-!function(e){var r={};function n(o){if(r[o])return r[o].exports;var u=r[o]={i:o,l:!1,exports:{}};return e[o].call(u.exports,u,u.exports,n),u.l=!0,u.exports}n.m=e,n.c=r,n.d=function(e,r,o){n.o(e,r)||Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:o})},n.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},n.p="",n(n.s="./examples/src/bundles/markdown.bundle.js")}({"./examples/src/bundles/markdown.bundle.js":
-/*!*************************************************!*\
-  !*** ./examples/src/bundles/markdown.bundle.js ***!
-  \*************************************************/
-/*! no static exports found */function(module,exports,__webpack_require__){eval('__webpack_require__( /*! ../md/markdown.md */ "./examples/src/md/markdown.md" );\n\n\n\n//# sourceURL=webpack:///./examples/src/bundles/markdown.bundle.js?')},"./examples/src/md/markdown.md":
-/*!*************************************!*\
-  !*** ./examples/src/md/markdown.md ***!
-  \*************************************/
-/*! no static exports found */function(module,exports,__webpack_require__){eval('module.exports = __webpack_require__.p + "../html/markdown.html";\n\n//# sourceURL=webpack:///./examples/src/md/markdown.md?')}});
